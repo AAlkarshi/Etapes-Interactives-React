b7016181a6c1a45d0c420d75ee5e911ff065ed1b
Abdullrahman Al karshi <alkarshi.abdullrahman@gmail.com>
2024-09-12 16:59:13 +0200

    Application en React qui permet de naviguer à travers trois étapes, chacune associée à un message différent. L'utilisateur peut passer d'une étape à l'autre à l'aide de boutons "Précédent" et "Suivant". Une fonctionnalité supplémentaire permet de masquer ou d'afficher l'interface en cliquant sur un bouton de fermeture. Le projet utilise le hook useState pour gérer l'état de l'étape actuelle ainsi que l'état d'ouverture de la fenêtre.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const messages = [
+  "Apprendre le React",
+  "Créer des Applications Mobiles avec React Native",
+  "Pour avoir + de Compétences",
+];
+
+export default function App() {
+  // 1er Valeur l'état actuel , 2ème valeur MAJ
+  const [etape, setEtape] = useState(1);
+
+  //Fenetre
+  const [FenetreOuvert , setFenetreOuvert] = useState(true)
+
+  //BTN TOUCHES
+  function touchePrécédent() {
+    if (etape > 1)  setEtape(etape - 1);
+  }
+  function toucheSuivant() {
+    if (etape < 3)  setEtape(etape + 1);
 }
 
-export default App;
+  return ( 
+    <div>
+      <button className="close" onClick={()=>setFenetreOuvert(!FenetreOuvert)}>&times;</button>
+
+    {FenetreOuvert && (
+      <div className="steps"> 
+        <div className="numbers">
+          <div className={etape >= 1 ? "active" : ""}> 1 </div>
+          <div className={etape >= 2 ? "active" : ""}> 2 </div>
+          <div className={etape >= 3 ? "active" : ""}> 3 </div>
+      </div>
+
+      <p className="message">
+        Etape {etape}: {messages[etape - 1]}
+      </p>
+
+    <div className="buttons">
+          <button style={{backgroundColor: "#7950F2" , color: "#fff"}} 
+                  onClick={(touchePrécédent)}> 
+            Précedent 
+          </button>
+
+          <button style={{backgroundColor: "#7950F2" , color: "#fff"}}
+                  onClick={(toucheSuivant)}> 
+            Suivant
+          </button>
+    </div>
+  
+  
+  </div>
+    )}
+  </div>
+  );
+}
